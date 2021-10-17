f99b03a89ea7cf666e10c1725353358aaaba2e63
Dmitry Asmalouski <dmitry.asmalouski@gmail.com>
2021-10-17 11:10:54 +0300

    added Api

--- src/main/java/com/example/demo/Account.java
+++ src/main/java/com/example/demo/Account.java
@@ -4,14 +4,5 @@ import lombok.Data;
 
 @Data
 public class Account {
-
     private final String iban;
-
-    public Account(String iban) {
-        this.iban = iban;
-    }
-
-    public String getIban() {
-        return iban;
-    }
 }
