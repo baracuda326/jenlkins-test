d5d1c58dac89e63ba2db08b88d2cf56e065ecf6d
Dmitry Asmalouski <dmitry.asmalouski@gmail.com>
2021-10-17 11:22:08 +0300

    added Api

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -4,6 +4,7 @@ package com.example.demo.controller;
 import com.example.demo.api.AccountsApiDelegate;
 import com.example.demo.model.GetAccountsResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController implements AccountsApiDelegate {
     private final AccountService accountService;
 
+    @Autowired
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
+
 
     @Override
     public ResponseEntity<GetAccountsResponseDto> getAccounts() {
