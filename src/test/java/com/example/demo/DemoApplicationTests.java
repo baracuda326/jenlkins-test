aa3c1e39c8854c315fb47715aac5e092fbc9f0f2
Dmitry Asmalouski <dmitry.asmalouski@gmail.com>
2021-10-17 09:20:03 +0300

    added Api

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,15 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(classes = {com.example.demo.DemoApplication.class})
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-		assertEquals(1, 1);
-	}
-
+    @Test
+    public void loadTest() {
+        assertEquals(1, 1);
+    }
 }
